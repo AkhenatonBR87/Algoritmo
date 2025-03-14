fafc075530b33caa32c110f68d00303ad25cdf98
AkhenatonBR87 <brasilakhenaton@gmail.com>
2025-03-13 21:54:57 -0300

    Create dimensoesretangulo-02.py

---
+++ dimensoesretangulo-02.py
@@ -0,0 +1,7 @@
+#02. Escreve um algoritmo para ler as dimensões de um retângulo (base e altura), calcular escrever a área do retângulo.
+print("Qual é a altura do retângulo?")
+altura=int(input())
+print("Qual o comprimento do retângulo?")
+comprimento=int(input())
+print("A área do retângulo é?")
+print(altura*comprimento)
