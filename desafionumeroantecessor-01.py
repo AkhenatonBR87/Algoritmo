4f44a80433f32629e5568f880b5f6502a334270f
AkhenatonBR87 <brasilakhenaton@gmail.com>
2025-03-13 21:36:20 -0300

    Create desafionumeroantecessor-01.py

---
+++ desafionumeroantecessor-01.py
@@ -0,0 +1,5 @@
+#01. Escreva um algoritmo para ler um valor numérico (do teclado) e escvrever (na tela) o seu antecessor.
+print("Digite um número")
+número=int(input())
+print("Seu número antecessor é?")
+print(número-1)
