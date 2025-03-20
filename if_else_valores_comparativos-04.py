f8484f435839153f2770627c2c76680402a254ad
Akhenaton Brasil <brasilakhenaton@gmail.com>
2025-03-20 19:41:44 -0300

    Create if_else_valores_comparativos-04.py
    #4 Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo).

---
+++ if_else_valores_comparativos-04.py
@@ -0,0 +1,7 @@
+#4 Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo). 
+print("Digite o valor a ser verificado")
+valor1=int(input())
+if(valor1>=0):
+    print("O valor inserido é positivo.")
+else:
+    print("O valor inserido é negativo.")
